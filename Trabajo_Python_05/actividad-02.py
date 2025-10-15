b27861a3aeb8a6463f53413e0d6edecb15c871b3
samy-canete <samyracanete@gmail.com>
2025-10-15 20:14:07 +0200

    Add files via upload

---
+++ Trabajo_Python_05/actividad-02.py
@@ -0,0 +1,6 @@
+horas =int(input("Ingrese la cantidad de horas trabajadas:"))
+tarifa =float(input("Ingrese la tarifa por hora:"))
+
+sueldo = horas * tarifa
+
+print("El sueldo total es: " + str(sueldo))
